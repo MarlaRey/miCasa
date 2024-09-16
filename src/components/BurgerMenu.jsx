47f821214166a8063808140e45ce308530b21e84
MarlaRey <hejmarlenehej@gmail.com>
2024-09-16 15:34:48 +0200

    burgermenu og resp på pageLayout

--- src/components/BurgerMenu.jsx
+++ src/components/BurgerMenu.jsx
@@ -1,13 +1,38 @@
-// BurgerMenu.jsx
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './BurgerMenu.module.scss';
 
-const BurgerMenu = ({ isMenuOpen, toggleMenu }) => {
+const BurgerMenu = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
-    <div className={styles.burgerMenu} onClick={toggleMenu}>
-      <div className={`${styles.bar} ${isMenuOpen ? styles.open : ''}`}></div>
-      <div className={`${styles.bar} ${isMenuOpen ? styles.open : ''}`}></div>
-      <div className={`${styles.bar} ${isMenuOpen ? styles.open : ''}`}></div>
+    <div className={styles.burgerMenu}>
+      {/* Ikon der skifter mellem tre linjer og et X */}
+      <div className={styles.icon} onClick={toggleMenu}>
+        <span className={`${styles.line} ${isMenuOpen ? styles.open : ''}`}></span>
+        <span className={`${styles.line} ${isMenuOpen ? styles.open : ''}`}></span>
+        <span className={`${styles.line} ${isMenuOpen ? styles.open : ''}`}></span>
+      </div>
+
+      {/* Menuen som folder sig ud */}
+      {isMenuOpen && (
+        <nav className={styles.menu}>
+          <ul>
+            <li><Link to="/" onClick={closeMenu}>Forside</Link></li>
+            <li><Link to="/boliger" onClick={closeMenu}>Boliger</Link></li>
+            <li><Link to="/kontakt" onClick={closeMenu}>Kontakt</Link></li>
+            <li><Link to="/login" onClick={closeMenu}>Login</Link></li>
+          </ul>
+        </nav>
+      )}
     </div>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,23 @@ import styles from './Header.module.scss';
 import { Link } from 'react-router-dom';
 import logo from '../assets/img/Logo.png'; // Tilpas stien til logoet
 import NavigationMenu from './NavigationMenu';
-import SearchBar from './SearchBar';
+import BurgerMenu from './BurgerMenu'; // Importér BurgerMenu
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.backgroundTop}></div>
-      <div className={styles.backgroundBottom}></div>
+      <div className={styles.background}>
+        <div className={styles.backgroundTop}></div>
+        <div className={styles.backgroundBottom}></div>
+      </div>
       <Link to="/" className={styles.logoLink}>
         <img src={logo} alt="Logo" className={styles.logo} />
       </Link>
       <NavigationMenu />
-      
-    
+      <BurgerMenu /> 
     </header>
   );
 };
 
 export default Header;
+
